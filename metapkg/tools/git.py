geldata/metapkg

commit 2c3950164943cba4b292ebef4eec840d3c312813

Upgrade Poetry to 2.1 (#38)
A bunch of API changes, some of which had to be re-implemented

---------

Co-authored-by: Fantix King <fantix.king@gmail.com>

metapkg/tools/git.py
@@ -1,22 +1,19 @@
 from __future__ import annotations
 from typing import (
-    TYPE_CHECKING,
     Any,
 )
 
 import os
 import pathlib
 import subprocess
+from functools import cached_property
 
 from dulwich import repo as dulwich_repo
 
 from poetry.core.vcs import git as core_git
 from poetry.vcs import git as poetry_git
 
-from . import cmd
-
-if TYPE_CHECKING:
-    from dulwich import repo as dulwich_repo
+from .cmd import cmd
 
 
 class Git(core_git.Git):
@@ -28,7 +25,7 @@ class Git(core_git.Git):
     ) -> str:
         if not folder and self._work_dir and self._work_dir.exists():
             folder = self._work_dir
-        result = cmd.cmd("git", *args, cwd=folder, **kwargs)
+        result = cmd("git", *args, cwd=folder, **kwargs)
         result = result.strip(" \n\t")
         return result
 
@@ -37,6 +34,60 @@ class Git(core_git.Git):
         work_tree = self._work_dir
         assert work_tree is not None
         return work_tree
+
+    @cached_property
+    def dulwich_repo(self) -> dulwich_repo.Repo:
+        assert self._work_dir is not None
+        return dulwich_repo.Repo(str(self._work_dir))
+
+    def rev_parse(self, rev: str) -> str:
+        repo = self.dulwich_repo
+        with repo:
+            return repo.get_peeled(rev.encode("utf-8")).decode("utf-8")
+
+    def peel_ref(self, ref: str) -> str:
+        if ref == "HEAD":
+            return self.rev_parse(ref)
+
+        # The name can be a branch or tag, so we attempt to look it up
+        # with ls-remote. If we don't find anything, we assume it's a
+        # commit hash.
+        rev = None
+        output = self.run("ls-remote", "--heads", "--tags", "origin", ref)
+        if output:
+            lines = output.splitlines()
+
+            sha_map: dict[str, str] = {}
+            for line in lines:
+                if not line.strip():
+                    continue
+                sha, name = line.strip().split("\t")
+                sha_map[name] = sha
+
+            # Try peeled tag (refs/tags/<ref>^{})
+            peeled_tag = f"refs/tags/{ref}^{{}}"
+            if peeled_tag in sha_map:
+                rev = sha_map[peeled_tag]
+
+            if rev is None:
+                # Try direct tag (refs/tags/<ref>)
+                tag = f"refs/tags/{ref}"
+                if tag in sha_map:
+                    rev = sha_map[tag]
+
+            if rev is None:
+                # Try branch (refs/heads/<ref>)
+                branch = f"refs/heads/{ref}"
+                if branch in sha_map:
+                    rev = sha_map[branch]
+
+        if rev is None:
+            # The name can be a branch or tag, so we attempt to look it up
+            # with ls-remote. If we don't find anything, we assume it's a
+            # commit hash.
+            rev = ref
+
+        return rev
 
 
 class GitBackend(poetry_git.Git):

metapkg/app.py
@@ -16,7 +16,7 @@ if TYPE_CHECKING:
     from cleo.io.outputs.output import Output
 
 
-class App(BaseApplication):  # type: ignore
+class App(BaseApplication):
     def __init__(self) -> None:
         super().__init__(metapkg.__name__, metapkg.__version__)
 
@@ -38,4 +38,4 @@ def main() -> int:
         cmd = getattr(metapkg_commands, cmd_name)
         app.add(cmd())
 
-    return app.run()  # type: ignore
+    return app.run()

metapkg/commands/base.py
@@ -1,5 +1,5 @@
 from cleo.commands import command as cleo_command
 
 
-class Command(cleo_command.Command):  # type: ignore
+class Command(cleo_command.Command):
     pass

metapkg/commands/build.py
@@ -10,9 +10,12 @@ import pathlib
 import sys
 import tempfile
 
-from poetry import mixology
+from cleo.helpers import argument, option
+
+from poetry import puzzle
 from poetry.core.packages import dependency as poetry_dep
 from poetry.core.packages import project_package
+from poetry.repositories import repository_pool as poetry_repository_pool
 from poetry.utils import env as poetry_env
 
 from metapkg import targets
@@ -24,32 +27,98 @@ from . import base
 
 
 class Build(base.Command):
-    """Build the specified package
-
-    build
-        { name : Package to build. }
-        { --jobs= : Use up to N processes in parallel to build. }
-        { --dest= : Destination path. }
-        { --keepwork : Do not remove the work directory. }
-        { --generic : Build a generic target. }
-        { --arch= : Target architecture, if different from host. }
-        { --libc= : Libc to target. }
-        { --build-source : Build source packages. }
-        { --build-debug : Build debug symbol packages. }
-        { --release : Whether this build is a release. }
-        { --source-ref= : Source version to build (VCS ref or tarball version). }
-        { --pkg-revision= : Override package revision number (defaults to 1). }
-        { --pkg-subdist= : Set package sub-distribution (e.g. nightly). }
-        { --pkg-tags= : Comma-separated list of key=value pairs to include in
-                        pckage metadata. }
-        { --pkg-compression= : Comma-separated list of compression encodings to
-                               apply if building for the specified target
-                               produces a tarball (defaults to gzip,zstd). }
-        { --extra-optimizations : Enable extra optimization
-                                  (increases build times). }
-    """
-
-    help = """Builds the specified package on the current platform."""
+    name = "build"
+    description = """Builds the specified package on the current platform."""
+    arguments = [
+        argument(
+            "name",
+            description="Package to build",
+        ),
+    ]
+    options = [
+        option(
+            "jobs",
+            description="Use up to N processes in parallel to build.",
+            flag=False,
+        ),
+        option(
+            "dest",
+            description="Destination path.",
+            flag=False,
+        ),
+        option(
+            "keepwork",
+            description="Do not remove the work directory upon exit.",
+            flag=True,
+        ),
+        option(
+            "generic",
+            description="Build a generic artifact.",
+            flag=True,
+        ),
+        option(
+            "arch",
+            description="Target architecture, if different from host.",
+            flag=False,
+        ),
+        option(
+            "libc",
+            description="Libc to target.",
+            flag=False,
+        ),
+        option(
+            "build-source",
+            description="Build source packages.",
+            flag=True,
+        ),
+        option(
+            "build-debug",
+            description="Build debug packages.",
+            flag=True,
+        ),
+        option(
+            "release",
+            description="Whether this build is a release.",
+            flag=True,
+        ),
+        option(
+            "source-ref",
+            description="Source version to build (VCS ref or tarball version).",
+            flag=False,
+        ),
+        option(
+            "pkg-revision",
+            description="Override package revision number (defaults to 1).",
+            flag=False,
+        ),
+        option(
+            "pkg-subdist",
+            description="Set package sub-distribution (e.g. nightly).",
+            flag=False,
+        ),
+        option(
+            "pkg-tags",
+            description=(
+                "Comma-separated list of key=value pairs to include "
+                "in package metadata."
+            ),
+            flag=False,
+        ),
+        option(
+            "pkg-compression",
+            description=(
+                "Comma-separated list of compression encodings to apply "
+                "if building for the specified target produces a tarball "
+                "(defaults to gzip,zstd)."
+            ),
+            flag=False,
+        ),
+        option(
+            "extra-optimizations",
+            description="Enable extra optimization (increases build times).",
+            flag=True,
+        ),
+    ]
 
     _loggers = ["metapkg.build"]
 
@@ -193,18 +262,26 @@ class Build(base.Command):
 
         repo_pool = af_repo.Pool()
         repo_pool.add_repository(target.get_package_repository())
-        repo_pool.add_repository(af_repo.bundle_repo, secondary=True)
+        repo_pool.add_repository(
+            af_repo.bundle_repo,
+            priority=poetry_repository_pool.Priority.SUPPLEMENTAL,
+        )
 
         item_repo = root_pkg.get_package_repository(target, io=self.io)
         if item_repo is not None and item_repo is not af_repo.bundle_repo:
-            repo_pool.add_repository(item_repo, secondary=True)
+            repo_pool.add_repository(
+                item_repo,
+                priority=poetry_repository_pool.Priority.SUPPLEMENTAL,
+            )
 
         provider = af_repo.Provider(root, repo_pool, self.io, extras=extras)
-        resolution = mixology.resolve_version(root, provider)
+        solver = puzzle.Solver(root, repo_pool, [], [], self.io)
+        solver._provider = provider
+        resolution = solver._solve()
 
         pkg_map: dict[mpkg_base.NormalizedName, mpkg_base.BasePackage] = {}
         graph = {}
-        for dep_package in resolution.packages:
+        for dep_package in resolution:
             pkg_map[dep_package.name] = cast(
                 mpkg_base.BasePackage, dep_package
             )
@@ -234,11 +311,17 @@ class Build(base.Command):
             include_build_reqs=True,
             extras=extras,
         )
-        resolution = mixology.resolve_version(build_root, provider)
+        solver = puzzle.Solver(build_root, repo_pool, [], [], self.io)
+        solver._provider = provider
+        mpkg_base.all_requires_include_build_reqs = True
+        try:
+            resolution = solver._solve()
+        finally:
+            mpkg_base.all_requires_include_build_reqs = False
 
         pkg_map = {}
         graph = {}
-        for dep_package in resolution.packages:
+        for dep_package in resolution:
             pkg_map[dep_package.name] = cast(
                 mpkg_base.BasePackage, dep_package
             )
@@ -316,6 +399,9 @@ class Build(base.Command):
         return reresolve_deps
 
     def _clamp_rlimit_nofile(self) -> None:
+        if sys.platform == "win32":
+            return
+
         try:
             import resource
         except ImportError:
@@ -324,7 +410,7 @@ class Build(base.Command):
             try:
                 fno_soft, fno_hard = resource.getrlimit(resource.RLIMIT_NOFILE)
             except resource.error:
-                self.io.write_warning_line("could not read RLIMIT_NOFILE")
+                self.io.write_error_line("could not read RLIMIT_NOFILE")
             else:
                 if fno_soft > 8192 or fno_hard > 8192:
                     try:
@@ -333,6 +419,6 @@ class Build(base.Command):
                             (min(8192, fno_soft), min(8192, fno_hard)),
                         )
                     except resource.error:
-                        self.io.write_warning_line(
+                        self.io.write_error_line(
                             "could not read RLIMIT_NOFILE"
                         )

metapkg/commands/metadata.py
@@ -5,6 +5,7 @@ import json
 import pathlib
 import sys
 
+from cleo.helpers import argument, option
 from poetry.utils import env as poetry_env
 
 from metapkg import targets
@@ -13,18 +14,41 @@ from . import base
 
 
 class Metadata(base.Command):
-    """Show metadata for a given package and version.
-
-    metadata
-        { name : Package to show metadata for. }
-        { --generic : Build a generic target. }
-        { --libc= : Libc to target. }
-        { --release : Whether this build is a release. }
-        { --source-ref= : Source version to build (VCS ref or tarball version). }
-        { --pkg-revision= : Override package revision number (defaults to 1). }
-    """
-
-    help = """Returns metadata for a given package and version."""
+    name = "metadata"
+    description = """Returns metadata for a given package and version"""
+    arguments = [
+        argument(
+            "name",
+            description="Package to build",
+        ),
+    ]
+    options = [
+        option(
+            "generic",
+            description="Build a generic artifact",
+            flag=True,
+        ),
+        option(
+            "libc",
+            description="Libc to target",
+            flag=False,
+        ),
+        option(
+            "release",
+            description="Whether this build is a release",
+            flag=True,
+        ),
+        option(
+            "source-ref",
+            description="Source version to build (VCS ref or tarball version)",
+            flag=False,
+        ),
+        option(
+            "pkg-revision",
+            description="Override package revision number (defaults to 1)",
+            flag=False,
+        ),
+    ]
 
     _loggers = ["metapkg.metadata"]
 

metapkg/packages/base.py
@@ -3,6 +3,7 @@ from typing import (
     TYPE_CHECKING,
     Any,
     ClassVar,
+    Iterable,
     Literal,
     Mapping,
     Sequence,
@@ -35,9 +36,8 @@ import packaging.utils
 from poetry.core.packages import dependency as poetry_dep
 from poetry.core.packages import dependency_group as poetry_depgroup
 from poetry.core.packages import package as poetry_pkg
-from poetry.core.semver import version as poetry_version
 from poetry.core.version import pep440 as poetry_pep440
-from poetry.core.constraints import version as poetry_constr
+from poetry.core.constraints import version as poetry_version
 from poetry.core.version.pep440 import segments as poetry_pep440_segments
 from poetry.core.spdx import helpers as poetry_spdx_helpers
 from poetry.repositories import exceptions as poetry_repo_exc
@@ -57,6 +57,7 @@ get_build_requirements = repository.get_build_requirements
 set_build_requirements = repository.set_build_requirements
 canonicalize_name = packaging.utils.canonicalize_name
 NormalizedName = packaging.utils.NormalizedName
+all_requires_include_build_reqs: bool = False
 
 
 Args: TypeAlias = dict[str, Union[str, pathlib.Path, None]]
@@ -85,7 +86,45 @@ class MetaPackage:
     dependencies: dict[str, str]
 
 
-class BasePackage(poetry_pkg.Package):
+class PackageWithPrettyVersion(poetry_pkg.Package):
+    def __init__(
+        self,
+        name: str,
+        version: str | poetry_version.Version,
+        source_type: str | None = None,
+        source_url: str | None = None,
+        source_reference: str | None = None,
+        source_resolved_reference: str | None = None,
+        source_subdirectory: str | None = None,
+        features: Iterable[str] | None = None,
+        develop: bool = False,
+        yanked: str | bool = False,
+        *,
+        pretty_version: str | None = None,
+    ) -> None:
+        super().__init__(
+            name=name,
+            version=version,
+            source_type=source_type,
+            source_url=source_url,
+            source_reference=source_reference,
+            source_resolved_reference=source_resolved_reference,
+            source_subdirectory=source_subdirectory,
+            features=features,
+            develop=develop,
+            yanked=yanked,
+        )
+
+        if pretty_version:
+            self._pretty_version = pretty_version
+        else:
+            if isinstance(version, poetry_version.Version):
+                self._pretty_version = version.text
+            else:
+                self._pretty_version = version
+
+
+class BasePackage(PackageWithPrettyVersion):
     @property
     def slot_suffix(self) -> str:
         return ""
@@ -424,6 +463,15 @@ class BasePackage(poetry_pkg.Package):
     def get_package_layout(self, build: targets.Build) -> PackageFileLayout:
         return PackageFileLayout.REGULAR
 
+    @property
+    def all_requires(
+        self,
+    ) -> list[poetry_dep.Dependency]:
+        if all_requires_include_build_reqs:
+            return super().all_requires + get_build_requirements(self)
+        else:
+            return super().all_requires
+
 
 @dataclasses.dataclass(kw_only=True)
 class PkgConfigMeta:
@@ -446,7 +494,7 @@ BundledPackage_T = TypeVar("BundledPackage_T", bound="BundledPackage")
 RequirementsSpec = Union[
     list[str | poetry_dep.Dependency],
     Mapping[
-        str | poetry_constr.VersionConstraint,
+        str | poetry_version.VersionConstraint,
         Sequence[str | poetry_dep.Dependency],
     ],
 ]
@@ -535,15 +583,15 @@ class BundledPackage(BasePackage):
                     **cls.get_source_url_variables(version),
                 )
                 extras = af_sources.SourceExtraDecl(
-                    source.get("extras", {})
-                )  # type: ignore
+                    source.get("extras", {})  # type: ignore
+                )
                 if extras:
                     if "version" not in extras:
                         extras["version"] = version
                 else:
                     extras = af_sources.SourceExtraDecl({"version": version})
 
-                if "vcs_version" not in extras:
+                if "vcs_version" not in extras and "version" in extras:
                     extras["vcs_version"] = cls.to_vcs_version(
                         extras["version"]
                     )
@@ -624,33 +672,6 @@ class BundledPackage(BasePackage):
         return tools.git.Git(repo_dir)
 
     @classmethod
-    def resolve_vcs_version(
-        cls,
-        io: cleo_io.IO,
-        repo: tools.git.Git,
-        version: str | None = None,
-    ) -> str:
-        rev: str
-
-        if version is None:
-            rev = repo.rev_parse("HEAD").strip()
-        else:
-            output = repo.run("ls-remote", repo.remote_url(), version)
-
-            if output:
-                rev, _ = output.split()
-                # If it's a tag, resolve the underlying commit.
-                if repo.run("cat-file", "-t", rev) == "tag":
-                    rev = repo.run("rev-list", "-n", "1", rev)
-            else:
-                # The name can be a branch or tag, so we attempt to look it up
-                # with ls-remote. If we don't find anything, we assume it's a
-                # commit hash.
-                rev = version
-
-        return rev
-
-    @classmethod
     def get_next_feature_version(
         cls,
         version: poetry_version.Version,
@@ -692,9 +713,9 @@ class BundledPackage(BasePackage):
                     local=None,
                     pre=None,
                     dev=poetry_pep440.ReleaseTag("dev", int(commits)),
-                ).to_string(short=False)
+                ).to_string()
             else:
-                ver = parsed_ver.to_string(short=False)
+                ver = parsed_ver.to_string()
 
         return ver
 
@@ -713,6 +734,7 @@ class BundledPackage(BasePackage):
         sources = list(cls._get_sources(version))
         vcs_source = cls.get_vcs_source(io, version)
         is_git = vcs_source is not None
+        git_date = ""
 
         if vcs_source is not None:
             sources[0] = vcs_source
@@ -721,7 +743,7 @@ class BundledPackage(BasePackage):
                 vcs_version = cls.to_vcs_version(version)
             else:
                 vcs_version = None
-            source_version = cls.resolve_vcs_version(io, repo, vcs_version)
+            source_version = repo.peel_ref(vcs_version or "HEAD")
             version = cls.version_from_vcs_version(
                 io, repo, source_version, is_release
             )
@@ -736,7 +758,6 @@ class BundledPackage(BasePackage):
             )
         elif version is not None:
             source_version = version
-            git_date = ""
         elif isinstance(sources[0], af_sources.LocalSource):
             source_dir = sources[0].url
             version = cls.version_from_source(pathlib.Path(source_dir))
@@ -855,9 +876,11 @@ class BundledPackage(BasePackage):
         reqs.extend(self.get_requirements())
 
         if reqs:
-            if poetry_depgroup.MAIN_GROUP not in self._dependency_groups:
-                self._dependency_groups[poetry_depgroup.MAIN_GROUP] = (
-                    poetry_depgroup.DependencyGroup(poetry_depgroup.MAIN_GROUP)
+            if not self.has_dependency_group(poetry_depgroup.MAIN_GROUP):
+                self.add_dependency_group(
+                    poetry_depgroup.DependencyGroup(
+                        poetry_depgroup.MAIN_GROUP
+                    ),
                 )
 
             main_group = self._dependency_groups[poetry_depgroup.MAIN_GROUP]
@@ -892,6 +915,10 @@ class BundledPackage(BasePackage):
                 )
                 repository.bundle_repo.add_package(pkg)
 
+    @property
+    def pretty_version(self) -> str:
+        return self._pretty_version
+
     def _get_requirements(
         self,
         spec: RequirementsSpec,
@@ -906,7 +933,7 @@ class BundledPackage(BasePackage):
         else:
             for ver, ver_reqs in spec.items():
                 if isinstance(ver, str):
-                    ver = poetry_constr.parse_constraint(ver)
+                    ver = poetry_version.parse_constraint(ver)
                 if ver.allows(self.version):
                     req_spec.extend(ver_reqs)
 
@@ -1229,7 +1256,7 @@ def merge_requirements(
     *specs: RequirementsSpec,
 ) -> RequirementsSpec:
     result: collections.defaultdict[
-        str | poetry_constr.VersionConstraint,
+        str | poetry_version.VersionConstraint,
         list[str | poetry_dep.Dependency],
     ] = collections.defaultdict(list)
     for spec in specs:
@@ -1261,7 +1288,7 @@ def _get_bundled_pkg_config_meta(name: str) -> PkgConfigMeta:
 def _get_pkg_in_bundle_repo(dep: poetry_dep.Dependency) -> poetry_pkg.Package:
     packages = repository.bundle_repo.find_packages(dep)
     if not packages:
-        raise poetry_repo_exc.PackageNotFound(
+        raise poetry_repo_exc.PackageNotFoundError(
             f"package {dep.pretty_name} not found in bundled repo."
         )
 
@@ -1569,7 +1596,7 @@ class BundledCAutoconfPackage(BundledCPackage):
         super().configure_dependency(build, dep, conf_args, conf_env, wd=wd)
         try:
             pkg_config_meta = self.get_dep_pkg_config_meta(dep)
-        except poetry_repo_exc.PackageNotFound:
+        except poetry_repo_exc.PackageNotFoundError:
             # This is a preinstalled system build-time package,
             # for which we have no in-tree definition.
             return

metapkg/packages/python.py
@@ -14,9 +14,10 @@ import textwrap
 
 from poetry.core.packages import dependency as poetry_dep
 from poetry.core.packages import package as poetry_pkg
-from poetry.core.semver import version as poetry_version
+from poetry.core.constraints import version as poetry_version
 from poetry.repositories import pypi_repository
 from poetry.repositories import exceptions as poetry_repo_exc
+from poetry.utils import cache as poetry_cache
 
 import build as pypa_build
 import build.env as pypa_build_env
@@ -55,6 +56,9 @@ class PyPiRepository(pypi_repository.PyPiRepository):
             "flit-core": FlitCore,
             "tomli": Tomli,
         }
+        self._dep_cache: poetry_cache.FileCache[list[str]] = (
+            poetry_cache.FileCache(path=self._cache_dir)
+        )
 
     def register_package_impl(
         self,
@@ -87,17 +91,14 @@ class PyPiRepository(pypi_repository.PyPiRepository):
         self,
         name: str,
         version: poetry_version.Version,
-        extras: list[str] | None = None,
     ) -> poetry_pkg.Package:
         if name.startswith("pypkg-"):
             name = name[len("pypkg-") :]
 
         try:
-            orig_package = super().package(
-                name=name, version=version, extras=extras
-            )
+            orig_package = super().package(name=name, version=version)
         except ValueError as e:
-            raise poetry_repo_exc.PackageNotFound(
+            raise poetry_repo_exc.PackageNotFoundError(
                 f"Package {name} ({version}) not found."
             ) from e
 
@@ -145,7 +146,7 @@ class PyPiRepository(pypi_repository.PyPiRepository):
         if self._disable_cache:
             build_reqs = self._get_build_requires(package)
         else:
-            build_reqs = self._cache.remember_forever(
+            build_reqs = self._dep_cache.remember(
                 f"{package.unique_name}:build-requirements",
                 lambda: self._get_build_requires(package),
             )
@@ -196,22 +197,9 @@ class PyPiRepository(pypi_repository.PyPiRepository):
     ) -> dict[str, Any]:
         if name.startswith("pypkg-"):
             name = name[len("pypkg-") :]
-        if self._disable_cache:
-            pypi_info = self._get_pypi_info(name, version)
-        else:
-            pypi_info = self._cache.remember_forever(
-                f"{name}:{version}:pypi-info",
-                lambda: self._get_pypi_info(name, version),
-            )
-
-        return pypi_info
-
-    def _get_pypi_info(
-        self, name: str, version: poetry_version.Version
-    ) -> dict[str, Any]:
         json_data = self._get(f"pypi/{name}/{version}/json")
         if json_data is None:
-            raise poetry_repo_exc.PackageNotFound(
+            raise poetry_repo_exc.PackageNotFoundError(
                 f"Package {name} ({version}) not found."
             )
         else:
@@ -637,7 +625,7 @@ class BundledPythonPackage(BasePythonPackage, base.BundledPackage):
             name=name,
             pretty_version=pretty_version,
             requires=requires,
-            source_version=cls.resolve_vcs_version(io, repo),
+            source_version=repo.rev_parse("HEAD"),
         )
         package.dist_name = dist.name
         repository.set_build_requirements(

commit 6b8b8457ab15839b3b63da6e4cf352ebdfdbd967

Fix peeling of annotated tags
Another regression from #38.

metapkg/tools/git.py
@@ -75,6 +75,13 @@ class Git(core_git.Git):
                 if tag in sha_map:
                     rev = sha_map[tag]
 
+                # If it's a tag object, peel it.
+                if (
+                    rev is not None
+                    and self.run("cat-file", "-t", rev) == "tag"
+                ):
+                    rev = self.run("rev-list", "-n", "1", rev)
+
             if rev is None:
                 # Try branch (refs/heads/<ref>)
                 branch = f"refs/heads/{ref}"